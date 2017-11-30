bad91bef5204ef70a4dce0e504b1123d4d0967b8
zhong <1246802560@qq.com>
2017-11-30 21:00:40 +0800

    Initial commit

---
+++ src/com/youmeeek/demo/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.youmeeek.demo;
+
+/**
+ * Created by zhong on 2017/11/30.
+ */
+public class HelloWorld {
+
+    public static void main(String[] args){
+        System.out.print("HelloWord");
+    }
+}
